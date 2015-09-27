1954913c45e9286282d815bb2c3741cf036c698d
pabloapast <pabloapast@gmail.com>
2015-09-27 19:07:15 -0300

    listo el etiquetador baseline

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 
 class BaselineTagger:
@@ -6,7 +7,27 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        self.counts = counts = defaultdict(defaultdict)
+        self.frequent_tag = ''
+        tag_count = defaultdict(int)
+
+        # Count of tags for word
+        for tagged_sent in tagged_sents:
+            for word, tag in tagged_sent:
+                try:
+                    counts[word][tag] += 1
+                except KeyError:
+                    counts[word][tag] = 1
+                tag_count[tag] += 1
+
+        # For each word, order tags by ocurrence
+        for word, tags in counts.items():
+            counts[word] = sorted(tags.items(), key=lambda tup: tup[1],
+                                  reverse=True)
+
+        self.frequent_tag = max(tag_count.items(), key=lambda tup: tup[1])[0]
+
+        # print(self.counts)
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +41,21 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+        tag = ''
+        if self.unknown(w):
+            tag = self.frequent_tag
+        else:
+            tag = self.counts[w][0][0]
+        return tag
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        unkn = False
+        tags = self.counts.get(w)
+
+        if tags is None:
+            unkn = True
+        return unkn
