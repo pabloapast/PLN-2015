9c39b2a199144fc55f80c014fed1bc1c9fee2a20
pabloapast <pabloapast@gmail.com>
2015-09-29 02:18:47 -0300

    script con estadisticas terminado

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -20,45 +20,55 @@ if __name__ == '__main__':
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = corpus.tagged_sents()
 
-    # compute the statistics
-    sents_count = 0
-    words_count = 0
-    tags_total_count = 0
-    vocabulary_count = 0
-    tags_count = 0
-    vocabulary = set()
-    tags = set()
-    tags_dict = defaultdict(int)
-    tags_words_dict = defaultdict(defaultdict)
-
+    # Compute the statistics
+    words_list = []  # All the words
+    tags_list = []  # All the tags
+    # For each word, returns a list of their tags
+    tags_by_word = defaultdict(set)
+    # For each tag, returns a dict with their words and occurrence
+    words_by_tags = defaultdict(lambda : defaultdict(int))
+    word_frequence = defaultdict(int)
     for sent in sents:
-        sents_count += 1
-        for word, tag in sent:
-            words_count += 1
-            tags_total_count += 1
-            vocabulary.add(word)
-            tags.add(tag)
-            tags_dict[tag] += 1
-            try:
-                tags_words_dict[tag][word] += 1
-            except KeyError:
-                tags_words_dict[tag][word] = 1
-    vocabulary_count = len(vocabulary)
-    tags_count = len(tags)
+        # words, tags = zip(*sent)
+        for w, t in sent:
+            tags_by_word[w].add(t)
+            words_by_tags[t][w] += 1
+            word_frequence[w] += 1
+            words_list.append(w)
+            tags_list.append(t)
+    words_set = set(words_list)
+    tags_set = set(tags_list)
 
-    print('Sents:', sents_count)
-    print('Words:', words_count)
-    print('Vocabulary:', vocabulary_count)
-    print('Tags set:', tags_count, end='\n\n')
+    # Basic stats
+    print('--- Basic Stats ---')
+    print('Sents: {}'.format(len(sents)))
+    print('Words: {}'.format(len(words_list)))
+    print('Vocabulary: {}'.format(len(words_set)))
+    print('Tags: {}'.format(len(tags_set)))
+    print('')
 
-    sorted_tags = sorted(tags_dict.items(), key=lambda tup: tup[1],
-                         reverse=True)[:10]
-    print('\n--- FREQUENT TAGS ---')
-    for tag, t_count in sorted_tags:
-        print('\n* ', tag, ': ', t_count, ', ',
-              "{:0.2f}".format((t_count * 100) / tags_total_count), '%', sep='')
+    # Frequent tags
+    top10_tags = sorted(words_by_tags.keys(),
+                        key=lambda k: sum(words_by_tags[k].values()),
+                        reverse=True)[:10]
+    print('--- Frequent Tags ---')
+    print(' Tag \tFrequence')
+    for tag in top10_tags:
+        frequence = sum(words_by_tags[tag].values())
+        frequent_words = sorted(words_by_tags[tag].keys(),
+                                key=lambda k: words_by_tags[tag][k],
+                                reverse=True)[:5]
+        print(' {} \t{} - {:2.2f}% = {}'.format(tag, frequence,
+              (frequence / len(tags_list)) * 100, frequent_words))
+    print('')
 
-        sorted_words = sorted(tags_words_dict[tag].items(),
-                              key=lambda tup: tup[1], reverse=True)[:5]
-        for word, w_count in sorted_words:
-            print('   - ', word, ': ', w_count, sep='')
+    # Ambiguity levels
+    print('--- Ambiguity Levels ---')
+    print(' Ambiguity \tWords')
+    for level in range(1, 10):
+        filtered_words = dict((word, t_list) for word, t_list in
+                              tags_by_word.items() if level == len(t_list))
+        top5_words = sorted(filtered_words.keys(),
+                            key=lambda w: word_frequence[w], reverse=True)[:5]
+        print(' {} tag  \t{} - {:2.2f}% = {}'.format(level, len(filtered_words),
+              (len(filtered_words) / len(words_set)) * 100, top5_words))
