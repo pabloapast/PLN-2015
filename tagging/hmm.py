4ea8c6ed1acd92b72c6adb4c89c82071d530e39e
pabloapast <pabloapast@gmail.com>
2015-10-06 01:11:52 -0300

    posible implementacion de virterbi dinamicamente

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from math import log2
 
 START = '<s>'
@@ -122,10 +123,55 @@ class ViterbiTagger:
         """
         hmm -- the HMM.
         """
+        self.hmm = hmm
 
+    def K_n(self, k):
+        if k - self.n <= 0:
+            return set([START])
+        else:
+            return self.hmm.tags
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
+        # llenar pi[0]
+        # for i in range(1, n+1):
+        #     llenar pi[i][t1, t2]
+        #     en terminos de pi[i-1]
+        #     # Opcion 1
+        #     for t1, t2 in tags^2:
+        #         llenar pi[i][t1,t2] en termino de pi[i-1]
+        #     # Opcion 2 - la buena
+        #     for t1, t2, w in pi[i-1].items()
+        #         llenar los pi[i] que correspondan (!= 0)
+        N = len(sent)
+        tag_secuence = defaultdict()
+        pi = defaultdict(defaultdict)
+        # bp = defaultdict(defaultdict)
+
+        pi[0][(START,) * self.n] = 1
+        for k in range(1, N + 1):
+            for tag in self.K_n(k):
+                max_prob = 0
+                for prev_tags, value in pi[k - 1].items():
+                    # tag = v, prev_tags = (w, u)
+                    # self.pi[k][prev_tags + tuple([tag])] = 0
+                    partial_prob = (value * self.trans_prob(tag, prev_tags) *
+                                    self.out_prob(sent[k], (tag,)))
+                    if max_prob < partial_prob:
+                        max_prob = partial_prob
+                        pi[k][prev_tags[1:] + (v,)] = max_prob
+                        tag_secuence[k] = prev_tags[0]
+                        # bp[k][prev_tags[1:] + (v,)] = prev_tags[0]
+
+        max_prob = 0
+        for prev_tags, value in pi[N].items():
+            partial_prob = (value * self.trans_prob(STOP, prev_tags))
+            if max_prob < partial_prob:
+                max_prob = partial_prob
+                for i, tag in enumerate(prev_tags):
+                    tag_secuence[N + 1 - self.n - i] = tag
+
+        return tag_secuence.values()
