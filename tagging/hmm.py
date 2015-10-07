2a101902022e5d158a7026a4641cbac65c7e8831
pabloapast <pabloapast@gmail.com>
2015-10-06 22:15:34 -0300

    cambiando cosas de viterbi, empezando HMM con maximum likelihood

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -35,11 +35,23 @@ class HMM:
             assert self.n == 1
             tag_prob = self.trans.get(tag, 0)
         else:
-            tags_with_probs = self.trans.get(tuple(prev_tags))  # PUEDE DEVOLVER NONE??
+            tags_with_probs = self.trans.get(tuple(prev_tags), dict())
             tag_prob = tags_with_probs.get(tag, 0)
 
         assert tag_prob >= 0
         return tag_prob
+
+    def trans_log_prob(self, tag, prev_tags):
+        """Log probability of a tag.
+
+        tag -- the tag.
+        prev_tags -- tuple with the previous n-1 tags (optional only if n = 1).
+        """
+        tag_prob = self.trans_prob(tag, prev_tags)
+        if tag_prob == 0:
+            return float('-inf')
+        else:
+            return log2(tag_prob)
 
     def out_prob(self, word, tag):
         """Probability of a word given a tag.
@@ -47,7 +59,21 @@ class HMM:
         word -- the word.
         tag -- the tag.
         """
-        return (self.out.get(tag)).get(word, 0)
+        # print(self.out, self.out.get(tag[0]))
+        return self.out.get(tag, dict()).get(word, 0)
+
+    def out_log_prob(self, word, tag):
+        """Log probability of a word given a tag.
+
+        word -- the word.
+        tag -- the tag.
+        """
+        word_prob = self.out_prob(word, tag)
+        # print(word, tag, word_prob)
+        if word_prob == 0:
+            return float('-inf')
+        else:
+            return log2(word_prob)
 
     def tag_prob(self, y):
         """
@@ -57,8 +83,8 @@ class HMM:
         y -- tagging.
         """
         prev_tags = [START] * (self.n - 1)
-        tagging = prev_tags + list(y)
-        p = 0
+        tagging = prev_tags + y
+        p = 1
         for i in range(self.n - 1, len(tagging)):
             p *= self.trans_prob(tagging[i], tagging[i - self.n + 1:i])
         return p
@@ -74,8 +100,8 @@ class HMM:
         assert len(x) == len(y)
 
         p_y = self.tag_prob(y)
-        p_x = 0
-        for i in len(x):
+        p_x = 1
+        for i in range(len(x)):
             p_x *= self.out_prob(x[i], y[i])
         return p_y * p_x
 
@@ -89,7 +115,7 @@ class HMM:
         tagging = prev_tags + list(y)
         p = 0
         for i in range(self.n - 1, len(tagging)):
-            p += log2(self.trans_prob(tagging[i], tagging[i - self.n + 1:i]))
+            p += self.trans_log_prob(tagging[i], tagging[i - (self.n - 1):i])
         return p
 
     def log_prob(self, x, y):
@@ -103,8 +129,8 @@ class HMM:
 
         p_y = self.tag_log_prob(y)
         p_x = 0
-        for i in len(x):
-            p_x += log2(self.out_prob(x[i], y[i]))
+        for i in range(len(x)):
+            p_x += self.out_log_prob(x[i], y[i])
         return p_y + p_x
 
 
@@ -124,9 +150,10 @@ class ViterbiTagger:
         hmm -- the HMM.
         """
         self.hmm = hmm
+        self._pi = defaultdict(defaultdict)
 
     def K_n(self, k):
-        if k - self.n <= 0:
+        if k - self.hmm.n <= 0:
             return set([START])
         else:
             return self.hmm.tags
@@ -147,31 +174,59 @@ class ViterbiTagger:
         #     for t1, t2, w in pi[i-1].items()
         #         llenar los pi[i] que correspondan (!= 0)
         N = len(sent)
-        tag_secuence = defaultdict()
-        pi = defaultdict(defaultdict)
-        # bp = defaultdict(defaultdict)
+        # Initialization: pi(0, *, ..., *) = 1
+        self._pi[0][(START,) * (self.hmm.n - 1)] = (log2(1), [])
 
-        pi[0][(START,) * self.n] = 1
         for k in range(1, N + 1):
-            for tag in self.K_n(k):
-                max_prob = 0
-                for prev_tags, value in pi[k - 1].items():
-                    # tag = v, prev_tags = (w, u)
-                    # self.pi[k][prev_tags + tuple([tag])] = 0
-                    partial_prob = (value * self.trans_prob(tag, prev_tags) *
-                                    self.out_prob(sent[k], (tag,)))
+            for tag in self.hmm.tags:
+                max_prob = float('-inf')
+                for prev_tags, (prob, tag_seq) in self._pi[k - 1].items():
+                    partial_prob = (prob +
+                                    self.hmm.trans_log_prob(tag, prev_tags) +
+                                    self.hmm.out_log_prob(sent[k - 1], tag))
+
+                    # print(prev_tags, tag, sent[k - 1])
+                    # print(partial_prob, prob, self.hmm.trans_log_prob(tag, prev_tags),
+                          # self.hmm.out_log_prob(sent[k - 1], tag))
+
                     if max_prob < partial_prob:
                         max_prob = partial_prob
-                        pi[k][prev_tags[1:] + (v,)] = max_prob
-                        tag_secuence[k] = prev_tags[0]
-                        # bp[k][prev_tags[1:] + (v,)] = prev_tags[0]
+                        try:
+                            self._pi[k][prev_tags[1:] + (tag,)][0] = max_prob
+                            self._pi[k][prev_tags[1:] + (tag,)][1].append(tag)
+                        except KeyError:
+                            self._pi[k][prev_tags[1:] + (tag,)] = (max_prob,
+                                self._pi[k - 1][prev_tags][1].append(tag))
+            print(self._pi)
 
-        max_prob = 0
-        for prev_tags, value in pi[N].items():
-            partial_prob = (value * self.trans_prob(STOP, prev_tags))
-            if max_prob < partial_prob:
-                max_prob = partial_prob
-                for i, tag in enumerate(prev_tags):
-                    tag_secuence[N + 1 - self.n - i] = tag
+        return max(self._pi[N].items(), key=lambda tup: tup[1][0])[1]
 
-        return tag_secuence.values()
+
+class MLHMM(HMM):
+
+    def __init__(self, n, tagged_sents, addone=True):
+        """
+        n -- order of the model.
+        tagged_sents -- training sentences, each one being a list of pairs.
+        addone -- whether to use addone smoothing (default: True).
+        """
+
+    def tcount(self, tokens):
+        """Count for an k-gram for k <= n.
+
+        tokens -- the k-gram tuple.
+        """
+
+    def unknown(self, w):
+        """Check if a word is unknown for the model.
+
+        w -- the word.
+        """
+
+    """
+       Todos los métodos de HMM.
+    """
+
+
+
+
