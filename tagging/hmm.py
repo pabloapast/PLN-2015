3f0345f0362f65ebbfc06794e82d3b260352337c
pabloapast <pabloapast@gmail.com>
2015-10-08 20:43:30 -0300

    viterbi pasando test, MLHMM casi terminado (falta addone)

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -59,7 +59,6 @@ class HMM:
         word -- the word.
         tag -- the tag.
         """
-        # print(self.out, self.out.get(tag[0]))
         return self.out.get(tag, dict()).get(word, 0)
 
     def out_log_prob(self, word, tag):
@@ -69,7 +68,6 @@ class HMM:
         tag -- the tag.
         """
         word_prob = self.out_prob(word, tag)
-        # print(word, tag, word_prob)
         if word_prob == 0:
             return float('-inf')
         else:
@@ -150,27 +148,15 @@ class ViterbiTagger:
         hmm -- the HMM.
         """
         self.hmm = hmm
-        self._pi = defaultdict(defaultdict)
-
-    def K_n(self, k):
-        if k - self.hmm.n <= 0:
-            return set([START])
-        else:
-            return self.hmm.tags
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
-        # llenar pi[0]
-        # for i in range(1, n+1):
-        #     llenar pi[i][t1, t2]
-        #     en terminos de pi[i-1]
-        #     for t1, t2, w in pi[i-1].items()
-        #         llenar los pi[i] que correspondan (!= 0)
         N = len(sent)
         # Initialization: pi(0, *, ..., *) = 1
+        self._pi = defaultdict(defaultdict)
         self._pi[0][(START,) * (self.hmm.n - 1)] = (log2(1), [])
 
         for k in range(1, N + 1):
@@ -180,41 +166,30 @@ class ViterbiTagger:
                     partial_prob = (prob +
                                     self.hmm.trans_log_prob(tag, prev_tags) +
                                     self.hmm.out_log_prob(sent[k - 1], tag))
-
-                    # print(prev_tags, tag, sent[k - 1])
-                    # print(partial_prob, prob, self.hmm.trans_log_prob(tag, prev_tags),
-                          # self.hmm.out_log_prob(sent[k - 1], tag))
-
                     if max_prob < partial_prob:
                         max_prob = partial_prob
-                        try:
-                            self._pi[k][prev_tags[1:] + (tag,)][0] = max_prob
-                            self._pi[k][prev_tags[1:] + (tag,)][1].append(tag)
-                        except KeyError:
-                            self._pi[k][prev_tags[1:] + (tag,)] = (max_prob,
-                                self._pi[k - 1][prev_tags][1].append(tag))
-            print(self._pi)
+                        self._pi[k][prev_tags[1:] + (tag,)] = (max_prob,
+                                self._pi[k - 1][prev_tags][1] + [tag])
 
-        return max(self._pi[N].items(), key=lambda tup: tup[1][0])[1]
+        return max(self._pi[N].items(), key=lambda t: t[1][0])[1][1]
 
 
 class MLHMM:
 
-    def __init__(self, n, tagged_sents, addone=True):  # TODO AddOne
+    def __init__(self, n, tagged_sents, addone=True):  # TODO AddOne sobre e(x|s)
         """
         n -- order of the model.
         tagged_sents -- training sentences, each one being a list of pairs.
         addone -- whether to use addone smoothing (default: True).
         """
         self.n = n
-        self.trans = defaultdict(defaultdict)
-        self.out = defaultdict(defaultdict)
-
         self.counts = counts = defaultdict(int)
+        self.out = defaultdict(defaultdict)
         self.vocabulary = vocabulary = []
-        self.vocabulary_size = vocabulary_size = 0
         self.tags_set = tags_set = []
-        self.tagset_size = tagset_size = 0
+
+        # Compute counts of tags and words, out probabilities, vocabulary and
+        # tags_set
         for sent in sents:
             words, tags = zip(*sent)
             vocabulary += words
@@ -225,36 +200,11 @@ class MLHMM:
                 ngram = tuple(tags[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
+
         vocabulary = set(vocabulary)
-        vocabulary_size = len(vocabulary)
         tags_set = set(tags_set)
-        tagset_size = len(tags_set)
-
-        self.compute_trans()
-        self.compute_out()
-
-    def compute_trans(self):  # TODO
-        """
-        Compute transition probabilities dictionary.
-        """
-        # for key, value in model.counts.items():
-        #     if len(key) == self.n:
-        #         prev_tokens = key[:-1]
-        #         token = key[-1]
-        #         self.probs[prev_tokens][token] = model.prob(token,
-        #                                                     list(prev_tokens))
-
-        # for key, value in self.probs.items():
-        #     self.sorted_probs[key] = sorted(list(value.items()),
-        #                                     key=lambda tup: (tup[1], tup[0]),
-        #                                     reverse=True)
-        pass
-
-    def compute_out(self):  # TODO
-        """
-        Compute output probabilities dictionary.
-        """
-        pass
+        # self.vocabulary_size = len(vocabulary)
+        # self.tagset_size = len(tags_set)
 
     def tcount(self, tokens):
         """Count for an k-gram for k <= n.
@@ -294,7 +244,11 @@ class MLHMM:
         tag -- the tag.
         prev_tags -- tuple with the previous n-1 tags (optional only if n = 1).
         """
-        pass
+        tag_prob = self.trans_prob(tag, prev_tags)
+        if tag_prob == 0:
+            return float('-inf')
+        else:
+            return log2(tag_prob)
 
     def out_prob(self, word, tag):
         """Probability of a word given a tag.
@@ -310,7 +264,11 @@ class MLHMM:
         word -- the word.
         tag -- the tag.
         """
-        pass
+        word_prob = self.out_prob(word, tag)
+        if word_prob == 0:
+            return float('-inf')
+        else:
+            return log2(word_prob)
 
     def tag_prob(self, y):
         """
