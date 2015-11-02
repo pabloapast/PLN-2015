a4e112dceb9ec1c71c29bb6bce8fdd03a1bc2902
pabloapast <pabloapast@gmail.com>
2015-11-02 01:21:01 -0300

    corregido cky parser y pasando todos los tests de upcfg

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -10,9 +10,7 @@ class CKYParser:
         assert grammar.is_binarised()
 
         self.grammar = grammar
-        self.start = str(self.grammar.start())
         # self.productions = {}
-
 
     def parse(self, sent):
         """Parse a sequence of terminals.
@@ -22,6 +20,7 @@ class CKYParser:
         self._pi = _pi = {}
         self._bp = _bp = {}
         grammar = self.grammar
+        start = str(grammar.start())
         n = len(sent)
 
         for i, word in enumerate(sent, start=1):
@@ -46,12 +45,23 @@ class CKYParser:
                         Y, Z = prod.rhs()
                         Y, Z = str(Y), str(Z)
                         logprob = prod.logprob()
-                        for s in range(i, j):  # OJO: revisar este range
+                        for s in range(i, j):
                             if Y in _pi[i, s].keys() and Z in _pi[s + 1, j].keys():
                                 new_prob = logprob + _pi[i, s][Y] + _pi[s + 1, j][Z]
                                 if X not in _pi[i, j] or new_prob > _pi[i, j][X]:
                                     _pi[i, j][X] = new_prob
-                                    t = list(_bp[i, s].values()) + list(_bp[s + 1, j].values())
-                                    _bp[i, j][X] = Tree(X, t)
+                                    sub_trees = list(_bp[i, s].values()) + list(_bp[s + 1, j].values())
+                                    _bp[i, j][X] = Tree(X, sub_trees)
 
-        return _pi[1, n][self.start], _bp[1, n][self.start]
+        # Fails to parse
+        if _bp[1, n] == {}:
+            _pi[1, n][start] = 0  # XXX revisar si va 0 u otra cosa !!
+            sub_trees = []
+            for k in range(1, n + 1):
+                sub_trees += list(_bp[k, k].values())
+            _bp[1, n][start] = Tree(start, sub_trees)
+
+        # print('Pi = ' , _pi, '\n\n')
+        # print('Bp = ', _bp, '\n\n')
+
+        return _pi[1, n][start], _bp[1, n][start]
--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -1,43 +1,44 @@
-from collections import defaultdict
+from nltk.grammar import induce_pcfg, nonterminals
+
+from parsing.cky_parser import CKYParser
+from parsing.util import lexicalize, unlexicalize
 
 
 class UPCFG:
     """Unlexicalized PCFG.
     """
 
-    def __init__(self, parsed_sents):
+    def __init__(self, parsed_sents, start='sentence'):
         """
         parsed_sents -- list of training trees.
         """
         # List of productions in Chomsky Normal Form
-        self.cnf_sents = cnf_sents = [] # no guardar
+        cnf_prods = []
         # Convert each tree to CNF and collapse unary
         for sent in parsed_sents:
-            cnf_sent = sent.copy()
-            cnf_sent = unlexicalize(cnf_sent)
-            cnf_sent.chomsky_normal_form()
-            cnf_sent.collapse_unary()
-            cnf_sents.append(cnf_sent)
+            cnf_tree = sent.copy(deep=True)
+            cnf_tree = unlexicalize(cnf_tree)
+            cnf_tree.chomsky_normal_form()
+            cnf_tree.collapse_unary()
+            cnf_prods += cnf_tree.productions()
 
-        # usar induce pcfg
-        self.counts = counts = defaultdict(lambda: defaultdict(int))
-        self.prods_list = prods_list = [sents.productions()
-                                        for sent in parsed_sents]
-        for prods in prods_list:
-            for prod in prods:
-                counts[prod.lhs()][prod.rhs()] += 1
-
+        start = nonterminals(start)[0]
+        self.grammar = induce_pcfg(start, cnf_prods)  # UPCFG grammar
 
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
-        p_prods = []  # List of probabilistic productions
-        for tree in self.parsed_sents:
-
-
+        return self.grammar.productions()
 
     def parse(self, tagged_sent):
         """Parse a tagged sentence.
 
         tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
         """
+        sent, tags = zip(*tagged_sent)
+        parser = CKYParser(self.grammar)  # CKY parser
+        log_p, tree = parser.parse(tags)
+        tree = lexicalize(tree, sent)
+
+        return tree
+
