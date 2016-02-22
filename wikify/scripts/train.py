32674eafcb4ee5909dbb55aaa1b74ba9daa331f4
pabloapast <pabloapast@gmail.com>
2016-02-22 17:18:31 -0300

    added disambiguation model to train

--- wikify/scripts/train.py
+++ wikify/scripts/train.py
@@ -1,14 +1,15 @@
 """Train components of wikify
 Usage:
-  train.py -m <model> -i <xml> [-r <ratio>] [-c <cores>] -o <file>
+  train.py -m <model> -i <file> [-r <r>] [-c <c>] -o <file>
   train.py -h | --help
 Options:
-  -m <model>    Model to use:
+  -m <model>    Model to train:
                   keyphraseness: Train the 'keyphraseness' ranking method
                   disambiguation: Train the disambiguation method
-  -r <ratio>    Ratio between number of words in an article and number of
-                keywords, used in Keyphraseness [default: 6%]
-  -c <cores>    Number of cpu cores to use [default: 4].
+  -i <file>     XML dump to train the model
+  -r <r>        Ratio between number of words in an article and number of
+                  keywords, used in Keyphraseness [default: 6%]
+  -c <c>        Number of cpu cores to use [default: 4].
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -16,18 +17,21 @@ from docopt import docopt
 import pickle
 
 from wikify.keyphraseness import Keyphraseness
+from wikify.disambiguation import Disambiguation
 
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    # load the data
-    file = './wiki-dump/mini/enwiki-train5gb'
+    # Corpus
+    xml = opts['-i']
 
-    # train the model
-    model = Keyphraseness(file)
+    # Train the model
+    if opts['-m'] == 'keyphraseness':
+        model = Keyphraseness(xml=xml, processes=int(opts['-c']))
+    elif opts['-m'] = 'disambiguation':
+        model = Disambiguation(xml=xml)
 
-    # save it
-    filename = './wiki-dump/mini/Keyphraseness'
-    with open(filename, 'wb') as f:
+    # Save it
+    with open(opts['-o'], 'wb') as f:
         pickle.dump(model, f)
